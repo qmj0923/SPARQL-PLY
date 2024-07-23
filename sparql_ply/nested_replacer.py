c56f099bd3b16493a446bce028e53540b0c60b05
qmj <616190492@qq.com>
2024-07-23 18:49:07 +0800

    feat: expand syntax form

---
+++ sparql_ply/nested_replacer.py
@@ -0,0 +1,342 @@
+'''
+Copyright (c) 2024 qmj0923
+https://github.com/qmj0923/SPARQL-PLY
+
+Replace substrings in a given string using nested replacement strategies.
+'''
+from __future__ import annotations
+import itertools
+from collections import defaultdict
+from copy import deepcopy
+from typing import (
+    List, Tuple, Dict, Callable, Optional, Union,
+)
+
+
+class ReplacerNode:
+    '''
+    A node in the replacer tree.
+    '''
+
+    def __init__(
+        self, key: Optional[str], remainder: List[str],
+        children: List[ReplacerNode]
+    ):
+        if len(remainder) != len(children) + 1:
+            raise ValueError(
+                'Remainder must have one more element than children.'
+            )
+        self.key = key
+        self.remainder = remainder
+        self.children = children
+
+    def __repr__(self) -> str:
+        return (
+            f'ReplacerNode(key={self.key}, remainder={self.remainder}, '
+            f'children={self.children})'
+        )
+
+    def replace(
+        self, handler_dict: Dict[
+            str, Union[
+                str, ReplacerNode, 
+                Callable[[ReplacerNode], ReplacerNode]
+            ]
+        ]
+    ) -> ReplacerNode:
+        '''
+        Replace nodes in the replacer tree using the given handler dictionary.
+
+        This method replaces nodes in the replacer tree based on the provided
+        handler dictionary, which maps each key to a handler. A handler can be
+        one of the following:
+        - A string: This will be used to construct a leaf node and replace
+            nodes with the same key.
+        - A ReplacerNode: A copy of the ReplacerNode will be used to replace
+            nodes with the same key.
+        - A callable: The callable will be called with the node as an argument
+            and should return a ReplacerNode.
+
+        Parameters
+        ----------
+        handler_dict
+            A dictionary mapping keys to handlers.
+
+        Returns
+        -------
+        ReplacerNode
+            The replaced replacer tree.
+
+        Raises
+        ------
+        ValueError
+            If an invalid handler type is encountered.
+        '''
+        def handle_node(node: ReplacerNode) -> ReplacerNode:
+            return ReplacerNode(
+                node.key, node.remainder.copy(),
+                [
+                    dfs(child, handler_dict)
+                    for child in node.children
+                ],
+            )
+
+        def dfs(node: ReplacerNode, handler_dict: Dict) -> ReplacerNode:
+            if node.key not in handler_dict:
+                return handle_node(node)
+
+            target = handler_dict[node.key]
+            if isinstance(target, str):
+                return ReplacerFactory.create_leaf(target)
+            elif isinstance(target, ReplacerNode):
+                return deepcopy(target)
+            elif callable(target):
+                return target(handle_node(node))
+            else:
+                raise ValueError(
+                    f'Invalid handler type: {type(target)}'
+                )
+
+        return dfs(self, handler_dict).compact()
+
+    def compact(self) -> ReplacerNode:
+        '''
+        Compact the replacer tree by merging any node whose key is None with
+        its parent node.
+        '''
+        def handle_node(node: ReplacerNode) -> ReplacerNode:
+            new_remainder = [node.remainder[0]]
+            new_children = []
+            for i, child in enumerate(node.children):
+                if child.key is None:
+                    last = new_remainder.pop()
+                    r = child.remainder.copy()
+                    r[0] = last + r[0]
+                    r[-1] = r[-1] + node.remainder[i + 1]
+                    new_remainder.extend(r)
+                    new_children.extend(child.children)
+                else:
+                    new_remainder.append(node.remainder[i + 1])
+                    new_children.append(child)
+            return ReplacerNode(node.key, new_remainder, new_children)
+
+        def dfs(node: ReplacerNode) -> ReplacerNode:
+            result = ReplacerNode(
+                node.key, node.remainder.copy(),
+                [dfs(child) for child in node.children],
+            )
+            return handle_node(result)
+
+        return dfs(self)
+
+    def get_key2spans(self) -> Dict[str, List[Tuple[int, int]]]:
+        '''
+        Return a dictionary mapping each key to a list of spans, where each
+        span is a tuple of (start, stop) indices in the original string.
+        '''
+        def dfs(node: ReplacerNode):
+            node_len = 0
+            key2spans = defaultdict(list)
+            for x, y in zip(node.remainder, node.children):
+                node_len += len(x)
+                key2spans_y, node_len_y = dfs(y)
+                for key, span_list in key2spans_y.items():
+                    key2spans[key].extend([
+                        (start + node_len, stop + node_len)
+                        for start, stop in span_list
+                    ])
+                node_len += node_len_y
+            node_len += len(node.remainder[-1])
+
+            if node.key is not None:
+                key2spans[node.key].append((0, node_len))
+            return key2spans, node_len
+
+        key2spans, _ = dfs(self)
+        result = {
+            key: sorted(span_list, key=lambda x: (x[1], -x[0]))
+            for key, span_list in key2spans.items()
+        }
+        return result
+
+    def to_str(self) -> str:
+        '''
+        Return a string that the replacer tree represents.
+        '''
+        if not self.children:
+            return self.remainder[0]
+        else:
+            result = ''
+            for x, y in zip(self.remainder, self.children):
+                result += x + y.to_str()
+            result += self.remainder[-1]
+            return result
+
+    def to_indented_repr(self, indent: int = 2) -> str:
+        '''
+        Return a string representation of the replacer tree with indentation.
+        '''
+        def dfs(node: ReplacerNode, depth: int) -> str:
+            result = (
+                f'{" " * (indent * depth)}ReplaceNode(\n'
+                f'{" " * (indent * (depth + 1))}key={node.key},\n'
+                f'{" " * (indent * (depth + 1))}remainder={node.remainder},\n'
+            )
+            if not node.children:
+                result += (
+                    f'{" " * (indent * (depth + 1))}children=[],\n'
+                    f'{" " * (indent * depth)})\n'
+                )
+                return result
+            else:
+                result += f'{" " * (indent * (depth + 1))}children=[\n'
+                for child in node.children:
+                    result += dfs(child, depth + 2)
+                result += (
+                    f'{" " * (indent * (depth + 1))}],\n'
+                    f'{" " * (indent * depth)})\n'
+                )
+                return result
+
+        return dfs(self, 0)
+
+
+class ReplacerFactory:
+    '''
+    A factory class for creating replacer trees.
+    '''
+
+    @staticmethod
+    def create_leaf(
+        content: str, key: Optional[str] = ...
+    ) -> ReplacerNode:
+        '''
+        Make a replacer leaf from a string and an optional key.
+        '''
+        if key is ...:
+            key = content
+        return ReplacerNode(key, [content], [])
+
+    @staticmethod
+    def create_tree(
+        content: str, key2spans: Dict[str, List[Tuple[int, int]]]
+    ) -> ReplacerNode:
+        '''
+        Make a replacer tree from a string and a dictionary of key to spans.
+        '''
+        def check_two_span(span1, span2):
+            '''
+            Check if two spans are disjoint or one contains the other.
+            '''
+            start1, stop1 = span1
+            start2, stop2 = span2
+            return (
+                stop1 <= start2 or stop2 <= start1
+                or (start1 <= start2 and stop2 <= stop1)
+                or (start2 <= start1 and stop1 <= stop2)
+            )
+
+        if any((
+            not check_two_span(span1, span2)
+            for span1, span2 in itertools.combinations(
+                itertools.chain.from_iterable(key2spans.values()), 2
+            )
+        )):
+            raise ValueError(
+                'Each pair of spans must be disjoint or one contains the other.'
+            )
+
+        node_list = sorted(
+            [
+                (key, start, stop)
+                for key, span_list in key2spans.items()
+                for start, stop in span_list
+            ],
+            key=lambda x: (x[2], -x[1]),
+        )
+        mono_stack = list()
+        for key, start, stop in node_list:
+            if (not mono_stack) or start >= mono_stack[-1][2]:
+                node = ReplacerNode(key, [content[start:stop]], [])
+            else:
+                remainder_r = list()
+                children_r = list()
+                last_s0 = stop
+                while mono_stack and start <= mono_stack[-1][1]:
+                    cn, s0, s1 = mono_stack.pop()
+                    remainder_r.append(content[s1:last_s0])
+                    children_r.append(cn)
+                    last_s0 = s0
+                remainder_r.append(content[start:last_s0])
+                node = ReplacerNode(
+                    key, list(reversed(remainder_r)), list(reversed(children_r))
+                )
+            mono_stack.append((node, start, stop))
+
+        remainder = list()
+        children = list()
+        last_s1 = 0
+        for node, s0, s1 in mono_stack:
+            remainder.append(content[last_s1:s0])
+            children.append(node)
+            last_s1 = s1
+        remainder.append(content[last_s1:])
+        root = ReplacerNode(None, remainder, children)
+        return root
+
+
+if __name__ == '__main__':
+    '''
+    Suppose we have a pseduo function call `foo(a, b)`.
+    We can construct a replacer tree for it as follows.
+    '''
+    content = 'foo(a, b)'
+    key2spans = {
+        'foo': [(0, 9)],
+        'a': [(4, 5)],
+        'b': [(7, 8)],
+    }
+    rt1 = ReplacerFactory.create_tree(content, key2spans)
+
+    '''
+    Example 1:
+    Replace the function's arguments with `i` and `j` to get another call
+    `foo(i, j)`.
+    '''
+    param_dict = {
+        'a': 'i',
+        'b': 'j',
+    }
+    new_rt1 = rt1.replace(param_dict)
+    print(new_rt1.to_str())
+    # print(new_rt1.to_indented_repr())
+    # print(new_rt1.get_key2spans())
+
+    '''
+    Example 2:
+    Suppose that the pseduo function call `foo(a, b)` will return a string
+    `<a,b>`, and we have variable x = "xxx" and z = "zzz". Now we want to get
+    the result of `foo(x, foo("yyy", z))`, which is `<"xxx", <"yyy", "zzz">>`.
+    '''
+    content = 'foo(x, foo("yyy", z))'
+    key2spans = {
+        'foo': [(0, 21), (7, 20)],
+        'x': [(4, 5)],
+        '"yyy"': [(11, 16)],
+        'z': [(18, 19)],
+    }
+    rt2 = ReplacerFactory.create_tree(content, key2spans)
+
+    def foo_handler(node: ReplacerNode) -> ReplacerNode:
+        remainder = ['<'] + [','] * (len(node.children) - 1) + ['>']
+        return ReplacerNode(None, remainder, node.children)
+
+    handler_dict = {
+        'foo': foo_handler,
+        'x': '"xxx"',
+        'z': '"zzz"',
+    }
+    result_rt2 = rt2.replace(handler_dict)
+    print(result_rt2.to_str())
+    # print(result_rt2.to_indented_repr())
+    # print(result_rt2.get_key2spans())
---
+++ sparql_ply/util.py
@@ -0,0 +1,355 @@
+'''
+Copyright (c) 2024 qmj0923
+https://github.com/qmj0923/SPARQL-PLY
+'''
+
+from __future__ import annotations
+
+from collections import defaultdict
+from collections.abc import Sequence
+from typing import (
+    List, Tuple, Set, Dict, Callable, Optional, Union, Type,
+)
+
+from sparql_ply.components import (
+    NodeTerm, PropertyPath, CollectionPath, BlankNodePath, TriplesPath,
+    Expression, GraphPattern, Query, ComponentWrapper, QueryComponent,
+)
+from sparql_ply.nested_replacer import (
+    ReplacerNode, ReplacerFactory,
+)
+from sparql_ply.sparql_yacc import parse as parse_sparql
+
+
+def traverse(
+    component: QueryComponent,
+    before: Callable[[QueryComponent], None] = lambda x: None,
+    after: Callable[[QueryComponent], None] = lambda x: None,
+    skip: Callable[[QueryComponent], bool] = lambda x: False,
+    prune: Callable[[QueryComponent], bool] = lambda x: False,
+):
+    if skip(component):
+        return
+    before(component)
+    if not prune(component):
+        if isinstance(component, NodeTerm):
+            pass
+        elif isinstance(component, (
+            PropertyPath, CollectionPath, Expression, GraphPattern
+        )):
+            for child in component.children:
+                traverse(child, before, after)
+        elif isinstance(component, BlankNodePath):
+            for p, ol in component.pred_obj_list:
+                traverse(p, before, after)
+                for o in ol:
+                    traverse(o, before, after)
+        elif isinstance(component, TriplesPath):
+            traverse(component.subj, before, after)
+            if component.pred_obj_list is not None:
+                for p, ol in component.pred_obj_list:
+                    traverse(p, before, after)
+                    for o in ol:
+                        traverse(o, before, after)
+        elif isinstance(component, Query):
+            if component.target is not None:
+                if isinstance(component.target, (NodeTerm, GraphPattern)):
+                    traverse(component.target, before, after)
+                elif isinstance(component.target, Sequence):
+                    for t in component.target:
+                        if isinstance(t, NodeTerm):
+                            traverse(component.target, before, after)
+                        elif isinstance(t, ComponentWrapper):
+                            traverse(t[0], before, after)
+                            traverse(t[1], before, after)
+                        else:
+                            raise NotImplementedError
+                else:
+                    raise NotImplementedError
+            if component.dataset is not None:
+                for d in component.dataset:
+                    traverse(d[-1], before, after)
+            if component.pattern is not None:
+                traverse(component.pattern, before, after)
+            if component.group_by is not None:
+                for x in component.group_by:
+                    if isinstance(x, ComponentWrapper):
+                        traverse(x[0], before, after)
+                        traverse(x[1], before, after)
+                    elif isinstance(x, (NodeTerm, Expression)):
+                        traverse(x, before, after)
+                    else:
+                        raise NotImplementedError
+            if component.having is not None:
+                for x in component.having:
+                    traverse(x, before, after)
+            if component.order_by is not None:
+                for x in component.order_by:
+                    if isinstance(x, ComponentWrapper):
+                        traverse(x[1], before, after)
+                    elif isinstance(x, (NodeTerm, Expression)):
+                        traverse(x, before, after)
+                    else:
+                        raise NotImplementedError
+            if component.limit is not None:
+                traverse(component.limit, before, after)
+            if component.offset is not None:
+                traverse(component.offset, before, after)
+            if component.values is not None:
+                for row in component.values:
+                    for x in row:
+                        traverse(x, before, after)
+    after(component)
+
+
+def collect_component(
+    component: QueryComponent,
+    cls: Union[Type[QueryComponent], Tuple[Type[QueryComponent], ...]],
+    typ: Optional[int] = None,
+    skip: Callable[[QueryComponent], bool] = lambda x: False,
+    prune: Callable[[QueryComponent], bool] = lambda x: False,
+) -> List[QueryComponent]:
+    def func(x: QueryComponent):
+        nonlocal res
+        if isinstance(x, cls) and (typ is None or x.type & typ):
+            res.append(x)
+
+    res = []
+    traverse(component, func, skip=skip, prune=prune)
+    return res
+
+
+class LabelGenerator:
+    def __init__(
+        self,
+        gen_func: Callable[[int], str] = lambda x: str(x),
+        skip_set: Optional[Set[str]] = None,
+        start: int = 0,
+    ):
+        self.gen_func = gen_func
+        self.skip_set = skip_set if skip_set is not None else set()
+        self.counter = start
+    
+    def __call__(self) -> str:
+        for _ in range(len(self.skip_set) + 1):
+            res = self.gen_func(self.counter)
+            self.counter += 1
+            if res not in self.skip_set:
+                break
+        return res
+
+
+class SyntaxFormExpander:
+    '''
+    Expand abbreviations for IRIs and triple patterns
+
+    https://www.w3.org/TR/2013/REC-sparql11-query-20130321/#sparqlExpandForms
+    '''
+
+    KEY_A = 'a'
+    KEY_PN = 'NodeTerm' + str(NodeTerm.PREFIXED_NAME)
+    KEY_IRIREF = 'NodeTerm' + str(NodeTerm.IRIREF)
+
+    def __init__(self, prefix_dict: Optional[dict[str, str]] = None):
+        self.key2spans: Dict[str, List[Tuple[int, int]]] = defaultdict(list)
+        self.prefix_dict: Dict[str, str] = (
+            prefix_dict if prefix_dict is not None else dict()
+        )
+        self.base: Optional[str] = None
+        self.blank_gen: Optional[LabelGenerator] = None
+
+    #################### expand node term ####################
+    
+    def _collect_prologue(self, query: Query):
+        if query.prologue is None:
+            return
+        for row in reversed(query.prologue):
+            if len(row) == 2:
+                self.base = str(row[1])
+            elif len(row) == 3:
+                prefix = str(row[1])[:-1]
+                self.prefix_dict[prefix] = str(row[2])
+            else:
+                raise NotImplementedError
+
+    def _record_node_span(self, x: QueryComponent):
+        if not isinstance(x, NodeTerm):
+            return
+        if x.type & NodeTerm.SPECIAL and x.value == 'a':
+            self.key2spans[self.KEY_A].append([x.lexstart, x.lexstop])
+        elif x.type & NodeTerm.PREFIXED_NAME:
+            self.key2spans[self.KEY_PN].append([x.lexstart, x.lexstop])
+        elif x.type & NodeTerm.IRIREF and self.base is not None:
+            self.key2spans[self.KEY_IRIREF].append([x.lexstart, x.lexstop])
+
+    def _prefixed_name_handler(self, node: ReplacerNode) -> ReplacerNode:
+        res = pn = node.remainder[0]
+        prefix, val = pn.split(':', 1)
+        if prefix in self.prefix_dict:
+            mapped_iri = self.prefix_dict[prefix][1:-1]
+            idx = mapped_iri.rfind('/')
+            mapped_iri = '/' if idx == -1 else mapped_iri[:idx + 1]
+            res = '<' + mapped_iri + val + '>'
+        else:
+            raise ValueError(f'Prefix {prefix} not found')
+        return ReplacerFactory.create_leaf(res, None)
+
+    def _iriref_handler(self, node: ReplacerNode) -> ReplacerNode:
+        res = iriref = node.remainder[0]
+        if self.base is not None:
+            mapped_iri = self.base[1:-1]
+            idx = mapped_iri.rfind('/')
+            mapped_iri = '/' if idx == -1 else mapped_iri[:idx + 1]
+            res = '<' + mapped_iri + iriref[1:-1] + '>'
+        return ReplacerFactory.create_leaf(res, None)
+
+    @classmethod
+    def expand_node_term(
+        cls, sparql: str,
+        default_prefix_dict: Optional[dict[str, str]] = None,
+    ) -> str:
+        '''
+        Expand abbreviations for IRIs.
+        '''
+        query = parse_sparql(sparql)
+        this = cls(default_prefix_dict)
+        this._collect_prologue(query)
+        traverse(query, this._record_node_span)
+        rt = ReplacerFactory.create_tree(sparql, this.key2spans)
+        handler_dict = {
+            cls.KEY_A: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>',
+            cls.KEY_PN: this._prefixed_name_handler,
+            cls.KEY_IRIREF: this._iriref_handler,
+        }
+        return rt.replace(handler_dict).to_str()
+
+    #################### expand triples path ####################
+    
+    def _process_object_node(
+        self, subj: NodeTerm, pred: Union[NodeTerm, PropertyPath],
+        obj_node: Union[NodeTerm, CollectionPath, BlankNodePath],
+    ) -> List[Tuple[
+        NodeTerm, Union[NodeTerm, PropertyPath], NodeTerm,
+    ]]:
+        if isinstance(obj_node, NodeTerm):
+            triples = [[subj, pred, obj_node]]
+        elif isinstance(obj_node, CollectionPath):
+            obj, nt_list = self._process_rdf_collection(obj_node)
+            triples = [[subj, pred, obj]] + nt_list
+        elif isinstance(obj_node, BlankNodePath):
+            obj, nt_list = self._process_blank_node(obj_node)
+            triples = [[subj, pred, obj]] + nt_list
+        else:
+            raise NotImplementedError
+        return triples
+
+    def _process_rdf_collection(
+        self, collection: CollectionPath,
+    ) -> Tuple[NodeTerm, List[Tuple[
+        NodeTerm, Union[NodeTerm, PropertyPath], NodeTerm,
+    ]]]:
+        RDF_FIRST = '<http://www.w3.org/1999/02/22-rdf-syntax-ns#first>'
+        RDF_REST = '<http://www.w3.org/1999/02/22-rdf-syntax-ns#rest>'
+        RDF_NIL = '<http://www.w3.org/1999/02/22-rdf-syntax-ns#nil>'
+        nt_first = NodeTerm(-1, -1, RDF_FIRST, NodeTerm.IRIREF)
+        nt_rest = NodeTerm(-1, -1, RDF_REST, NodeTerm.IRIREF)
+        nt_nil = NodeTerm(-1, -1, RDF_NIL, NodeTerm.IRIREF)
+        
+        subj = NodeTerm(-1, -1, self.blank_gen(), NodeTerm.BLANK_NODE_LABEL)
+        triples = list()
+        last = subj
+        for i, x in enumerate(collection.children):
+            triples += self._process_object_node(last, nt_first, x)
+
+            if i == len(collection.children) - 1:
+                triples.append([last, nt_rest, nt_nil])
+                break
+            else:
+                obj = NodeTerm(
+                    -1, -1, self.blank_gen(),
+                    NodeTerm.BLANK_NODE_LABEL
+                )
+                triples.append([last, nt_rest, obj])
+                last = obj
+        return subj, triples
+
+    def _process_blank_node(
+        self, blank_node: BlankNodePath,
+    ) -> Tuple[NodeTerm, List[Tuple[
+        NodeTerm, Union[NodeTerm, PropertyPath], NodeTerm,
+    ]]]:
+        subj = NodeTerm(-1, -1, self.blank_gen(), NodeTerm.BLANK_NODE_LABEL)
+        triples = list()
+        for pred, obj_list in blank_node.pred_obj_list:
+            for obj in obj_list:
+                triples += self._process_object_node(subj, pred, obj)
+        return subj, triples
+
+    def _process_triples_path(
+        self, triples_path: TriplesPath
+    ) -> List[Tuple[
+        NodeTerm, Union[NodeTerm, PropertyPath], NodeTerm,
+    ]]:
+        if isinstance(triples_path.subj, NodeTerm):
+            subj, triples = triples_path.subj, list()
+        elif isinstance(triples_path.subj, CollectionPath):
+            subj, triples = self._process_rdf_collection(triples_path.subj)
+        elif isinstance(triples_path.subj, BlankNodePath):
+            subj, triples = self._process_blank_node(triples_path.subj)
+        else:
+            raise NotImplementedError
+        
+        if triples_path.pred_obj_list is None:
+            return triples
+        for pred, obj_list in triples_path.pred_obj_list:
+            for obj in obj_list:
+                triples += self._process_object_node(subj, pred, obj)
+        return triples
+
+    @classmethod
+    def expand_triples_path(cls, sparql: str) -> str:
+        '''
+        Expand abbreviations for triple patterns.
+        '''
+        query = parse_sparql(sparql)
+        this = cls()
+        blank_labels = collect_component(
+            query, NodeTerm, NodeTerm.BLANK_NODE_LABEL
+        )
+        this.blank_gen = LabelGenerator(
+            lambda x: f'_:b{x}',
+            {str(x) for x in blank_labels},
+        )
+
+        tp_list = collect_component(
+            query, TriplesPath, 
+            prune=lambda x: isinstance(x, TriplesPath),
+        )
+        expanded_list = list()
+        for tp in tp_list:
+            triples = this._process_triples_path(tp)
+            new_str = '.\n'.join(
+                ' '.join(str(x) for x in triple)
+                for triple in triples
+            )
+            expanded_list.append(new_str)
+
+        tp_spans = [[tp.lexstart, tp.lexstop] for tp in tp_list]
+        key2spans = {str(i): [span] for i, span in enumerate(tp_spans)}
+        handler_dict = {str(i): x for i, x in enumerate(expanded_list)}
+        rt = ReplacerFactory.create_tree(sparql, key2spans)
+        return rt.replace(handler_dict).to_str()
+
+    ############################################################
+
+    @classmethod
+    def run(
+        cls, sparql: str,
+        default_prefix_dict: Optional[dict[str, str]] = None,
+    ) -> str:
+        '''
+        Expand abbreviations for IRIs and triple patterns.
+        '''
+        sparql = cls.expand_node_term(sparql, default_prefix_dict)
+        return cls.expand_triples_path(sparql)
+
